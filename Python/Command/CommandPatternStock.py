ae50caa565f6cb2acf3f9f1cdaf53bfeb0fca87a
Eduardo Petrolini Calzeta <eduardo@Eduardos-MacBook-Air.local>
2019-02-18 17:34:32 -0300

    More Design Patterns

---
+++ Python/Command/CommandPatternStock.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractmethod
+
+# COMMAND
+class Order(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def execute(self):
+        pass
+
+# CONCRET COMMAND
+class BuyStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    
+    def execute(self):
+        self.stock.buy()
+
+# CONCRET COMMAND
+class SellStockOrder(Order):
+    def __init__(self, stock):
+        self.stock = stock
+    
+    def execute(self):
+        self.stock.sell()
+
+# RECEIVER
+class StockTrade:
+    def buy(self):
+        print("You will buy stocks")
+    
+    def sell(self):
+        print("You will sell stocks")
+
+# INVOKER
+class Agent:
+    def __init__(self):
+        self.__orderQueue = []
+    
+    def placeOrder(self, order):
+        self.__orderQueue.append(order)
+        order.execute()
+
+if __name__ == '__main__':
+    #Client
+    stock = StockTrade()
+    buyStock = BuyStockOrder(stock)
+    sellStock = SellStockOrder(stock)
+    
+    #Invoker
+    agent = Agent()
+    agent.placeOrder(buyStock)
+    agent.placeOrder(sellStock)
